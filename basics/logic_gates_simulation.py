4a8b35f72f1d560c19760170a287e51b12782e37
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-11-10 23:57:34 +0000

    Made some classes for the logic gate sim to seperate binary and unary gates

--- basics/logic_gates_simulation.py
+++ basics/logic_gates_simulation.py
@@ -1,1 +1,17 @@
+# Logic gates simulation
+# AND gates have two input lines, each can be 0 or 1. If both = 1 then output 1 else 0
+# OR gates have two input lines, produce a 1 if one or both input lines = 1, if both = 0 result is 0
+# NOT gates have one input line, output value is the opposite of the input value, 1 = 0, 0 = 1
+# Each gate has a truth table to show the input-to-output mapping
+# Gates are split into 2 classes Binary Gates and Unary Gates
 
+class LogicGate:
+    def __init__(self, inp1, inp2):
+        self.input = 
+
+    def output():
+        return None
+
+class BinaryGate(LogicGate):
+
+class UnaryGate(LogicGate):
