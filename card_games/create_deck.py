7eaf9a93cc46c1ee7b83c19dff8901d0fd714dba
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-11-12 19:29:38 +0000

    Created a card gard where 4 people can race with suits

--- card_games/create_deck.py
+++ card_games/create_deck.py
@@ -6,14 +6,16 @@ class Card:
         self.csuit = csuit
         self.ctype = None
         self.colour = None
+        self.symbol = None
         
         self.setType()
         self.setColour()
+        self.setSymbol()
 
     def __repr__(self):
         rep = 'Card(' + str(self.num) + ', ' + str(self.ctype) + ', ' + str(self.csuit) + ', ' + str(self.colour) + ')'
         return rep
-     
+
     def setType(self):
         if self.num == 1:
             self.ctype = 'Ace'
@@ -24,13 +26,27 @@ class Card:
         elif self.num == 13:
             self.ctype = 'King'
         else:
-            self.ctype = 'Number'
+            self.ctype = f'{self.num}'
 
     def setColour(self):
+
         if self.csuit == 'Diamonds' or self.csuit == 'Hearts':
             self.colour = 'red'
         else:
             self.colour = 'black'
+    
+    def setSymbol(self):
+
+        s = self.ctype[0]
+
+        if self.csuit == 'Diamonds':
+            self.symbol = f'{s}♦'
+        elif self.csuit == 'Hearts':
+            self.symbol = f'{s}♥'
+        elif self.csuit == 'Clubs':
+            self.symbol = f'{s}♣'
+        else:
+            self.symbol = f'{s}♠'
         
 
 class JokerCard(Card):
@@ -39,18 +55,23 @@ class JokerCard(Card):
 
 
 class Deck:
-    def __init__(self, suits, joker=False):
+    def __init__(self, suits, size=1, joker=False):
         self.suits = suits
         self.joker = joker
         self.deck = None
+        self.size = size
 
     def createDeck(self):
         suits = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
         deck = []
+        cnt = self.size
 
-        for suit in suits:
-            for i in range(1, 14):
-                deck.append(Card(i, suit))
+        while cnt != 0:
+            for suit in suits:
+                for i in range(1, 14):
+                    deck.append(Card(i, suit))
+            cnt -= 1
+                    
 
         self.deck = deck
 
--- card_games/horses.py
+++ card_games/horses.py
@@ -1,33 +1,40 @@
+import os
 from create_deck import Deck
 from symbols import *
 
 class HorsesGame:
-    def __init__(self, players):
+    def __init__(self, players, length, deck_size=1):
         self.players = players
+        self.length = length
+        self.deck_size = deck_size
 
     def StartGame(self):
-        run = True
-        newdeck = Deck(self.players, joker=False)
+        newdeck = Deck(self.players, size=self.deck_size, joker=False)
         newdeck.createDeck()
         newdeck.shuffleDeck()
 
         scores = {
-            'Diamonds': 0,
-            'Hearts': 0,
-            'Clubs': 0,
-            'Spades': 0,
-        } 
+            'rounds': ["Rounds  ", "\t".join([str(n) for n in range(1, self.length+1)])],
+            'Diamonds': ['Diamonds'],
+            'Hearts': ['Hearts  '],
+            'Clubs': ['Clubs   '],
+            'Spades': ['Spades  '],
+        }
         
         for card in newdeck.deck:
-            if 10 not in scores.values():
-                scores[card.csuit] += 1
-            else:
-                break
-    
-        print(scores)
+            if (self.length+1) not in [len(i) for i in scores.values()]:
 
+                os.system('cls')
+                print(title)
 
-horses = HorsesGame(4)
+                scores[card.csuit].append(card.symbol)
+                for i in scores.values():
+                    print("" + "\t".join(i))
+
+                input("")
+        
+
+horses = HorsesGame(4, 10, 2)
 horses.StartGame()
 
-print(title)
+
